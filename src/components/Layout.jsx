b97e7bea2075b627cfe294f09c32eaa5fdb64408
AKEKOFranck <142504737+MauriceAkeko@users.noreply.github.com>
2025-10-14 03:50:27 +0000

    fourth test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import Like from '../components/Like';
 
 
 export default function Layout() {
@@ -11,7 +10,6 @@ export default function Layout() {
      <Header/>
       <main>
         <Outlet  />
-        <Like/>
       </main>
      <Footer/>
     </div>
